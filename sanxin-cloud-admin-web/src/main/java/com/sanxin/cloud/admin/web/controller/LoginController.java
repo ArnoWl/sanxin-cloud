a27be0f507bf5b4f2589ff867bce333a69bf859e
arno <bleachpjj@163.com>
2019-08-23 19:00:25 +0800

    Sanxin cloud

--- sanxin-cloud-admin-web/src/main/java/com/sanxin/cloud/admin/web/controller/LoginController.java
+++ sanxin-cloud-admin-web/src/main/java/com/sanxin/cloud/admin/web/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.sanxin.cloud.admin.web.controller;
 import com.sanxin.cloud.admin.api.service.LoginService;
 import com.sanxin.cloud.common.rest.RestResult;
 import com.sanxin.cloud.entity.SysConfig;
-import com.sanxin.cloud.mapper.SysConfigMapper;
-import com.sanxin.cloud.service.ISysConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
