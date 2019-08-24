2a7cb7f5b535183306fc3c338bb82ac657ecb8da
arno <bleachpjj@163.com>
2019-08-24 11:59:08 +0800

    Sanxin cloud

--- sanxin-cloud-admin-web/src/main/java/com/sanxin/cloud/admin/web/controller/LoginController.java
+++ sanxin-cloud-admin-web/src/main/java/com/sanxin/cloud/admin/web/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.sanxin.cloud.admin.api.service.LoginService;
 import com.sanxin.cloud.common.rest.RestResult;
 import com.sanxin.cloud.config.redis.RedisUtilsService;
 import com.sanxin.cloud.entity.SysConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +19,16 @@ import java.util.List;
  */
 @RestController
 public class LoginController {
+    protected final static Logger logger = LoggerFactory.getLogger(RedisUtilsService.class);
 
     @Autowired
     private LoginService loginService;
     @Autowired
     private RedisUtilsService redisUtilsService;
 
-
-
     @PostMapping("/loginOut")
     public RestResult loginOut(){
+        logger.info("输出sss");
         redisUtilsService.setKey("aaa","哈哈哈哈");
         System.out.print(redisUtilsService.getKey("aaa"));
         List<SysConfig> list=loginService.queryList();
