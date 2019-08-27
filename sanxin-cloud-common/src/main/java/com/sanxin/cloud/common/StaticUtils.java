25d1aef73be197f6f85b75ea9572462db0dde93e
sky-xiao <15574814866@163.com>
2019-08-27 15:26:24 +0800

    status update

--- sanxin-cloud-common/src/main/java/com/sanxin/cloud/common/StaticUtils.java
+++ sanxin-cloud-common/src/main/java/com/sanxin/cloud/common/StaticUtils.java
@@ -8,8 +8,28 @@ package com.sanxin.cloud.common;
 public class StaticUtils {
 
     /**
-     * 是否有效 0有效 -1无效
+     * 是否有效 1有效 0无效
      */
-    public static final Integer SATUS_YES=0;
-    public static final Integer SATUS_NO=-1;
+    public static final Integer SATUS_YES=1;
+    public static final Integer SATUS_NO=0;
+
+    /**
+     * 申请状态，申请中(val=1)
+     */
+    public static final int STATUS_APPLY = 1;
+
+    /**
+     * 申请状态，审核通过(val=2)
+     */
+    public static final int STATUS_SUCCESS = 2;
+
+    /**
+     * 申请状态，审核驳回(val=3)
+     */
+    public static final int STATUS_FAIL = 3;
+
+    /**
+     * 申请状态，超时(val=4)
+     */
+    public static final int STATUS_TIME_OUT = 4;
 }
