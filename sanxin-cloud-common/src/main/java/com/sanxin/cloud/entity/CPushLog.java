b72e646bde4365793e03fcbdbbcb9f1cdfa9b81b
854863723@qq.com <Zz809746598>
2019-09-19 10:16:01 +0800

    app

---
+++ sanxin-cloud-common/src/main/java/com/sanxin/cloud/entity/CPushLog.java
@@ -0,0 +1,164 @@
+package com.sanxin.cloud.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 消息表
+ * </p>
+ *
+ * @author Arno
+ * @since 2019-09-17
+ */
+public class CPushLog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    private Integer cid;
+
+    /**
+     * 交易单号
+     */
+    private String payCode;
+
+    /**
+     * 消息类型  见MsgUtils工具类
+     */
+    private String msgType;
+
+    /**
+     * 消息内容
+     */
+    private String content;
+
+    /**
+     * 外键 可能用来处理事件需要的参数
+     */
+    private String targetCode;
+
+    /**
+     * 0未读 1已读
+     */
+    private Integer reading;
+
+    private Date createTime;
+
+    /**
+     * 中文全内容
+     */
+    private String remark;
+
+    /**
+     * 逗号分隔内容
+     */
+    private String params;
+
+    /**
+     * 消息对象类型  标识用户消息或加盟商消息  见CashTypeEnums
+     */
+    private Integer targetType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+    public String getPayCode() {
+        return payCode;
+    }
+
+    public void setPayCode(String payCode) {
+        this.payCode = payCode;
+    }
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getTargetCode() {
+        return targetCode;
+    }
+
+    public void setTargetCode(String targetCode) {
+        this.targetCode = targetCode;
+    }
+    public Integer getReading() {
+        return reading;
+    }
+
+    public void setReading(Integer reading) {
+        this.reading = reading;
+    }
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+    public Integer getTargetType() {
+        return targetType;
+    }
+
+    public void setTargetType(Integer targetType) {
+        this.targetType = targetType;
+    }
+
+    @Override
+    public String toString() {
+        return "CPushLog{" +
+        "id=" + id +
+        ", cid=" + cid +
+        ", payCode=" + payCode +
+        ", msgType=" + msgType +
+        ", content=" + content +
+        ", targetCode=" + targetCode +
+        ", reading=" + reading +
+        ", createTime=" + createTime +
+        ", remark=" + remark +
+        ", params=" + params +
+        ", targetType=" + targetType +
+        "}";
+    }
+}
---
+++ sanxin-cloud-common/src/main/java/com/sanxin/cloud/mapper/CPushLogMapper.java
@@ -0,0 +1,16 @@
+package com.sanxin.cloud.mapper;
+
+import com.sanxin.cloud.entity.CPushLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 消息表 Mapper 接口
+ * </p>
+ *
+ * @author Arno
+ * @since 2019-09-17
+ */
+public interface CPushLogMapper extends BaseMapper<CPushLog> {
+
+}
