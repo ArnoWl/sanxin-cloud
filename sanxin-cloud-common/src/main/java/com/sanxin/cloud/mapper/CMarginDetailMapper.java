8799eb2b32a26a490f80bbc7791c78b4d7ac79c4
854863723@qq.com <Zz809746598>
2019-09-20 10:58:54 +0800

    app

---
+++ sanxin-cloud-common/src/main/java/com/sanxin/cloud/mapper/CMarginDetailMapper.java
@@ -0,0 +1,16 @@
+package com.sanxin.cloud.mapper;
+
+import com.sanxin.cloud.entity.CMarginDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 押金明细 Mapper 接口
+ * </p>
+ *
+ * @author Arno
+ * @since 2019-09-19
+ */
+public interface CMarginDetailMapper extends BaseMapper<CMarginDetail> {
+
+}
